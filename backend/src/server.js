0ebffc3de8dc0db950413d0087e8b97b241237e2
nithish-github07 <nithi4527@gmail.com>
2026-01-28 10:47:37 +0530

    Connected with mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,14 @@
 import app from "./app.js";
-import {config} from "./config/env.js";
+// import {config} from "./config/env.js";
+import connectDB from "./config/db.js";
+
+import dotenv from "dotenv"
+dotenv.config()
+
+export const config = {
+    port: process.env.PORT || 5000
+};
+connectDB();
 
 app.listen(config.port, ()=>{
     console.log(`Server running on port ${config.port}`);
