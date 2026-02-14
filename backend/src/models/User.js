7ac90666fcfb213fa5e09120ea2948ae843ec997
nithish-github07 <nithi4527@gmail.com>
2026-02-14 21:10:17 +0530

    Implement userProfile endpoints and skill management

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
+        // ========== BASIC INFO ==========
         name: {
             type: String,
             required: true,
@@ -23,7 +24,7 @@ const userSchema = new mongoose.Schema(
             default: "learner",
         },
 
-        //OTP/ verification
+        // ========== OTP/ VERIFICATION ==========
         isVerified: {
             type: Boolean,
             default: false
@@ -35,6 +36,80 @@ const userSchema = new mongoose.Schema(
 
         otpExpires: {
             type: Date
+        },
+
+        // ========== USER PROFILE INFO ==========
+        careerAspiration: {
+            targetJobRole: {
+                type: String,
+            },
+            targetSector: {
+                type: String,
+            },
+        },
+
+        engagementStatus: {
+            type: String,
+            enum: ["student", "employed", "self-employed", "unemployed", "apprentice"],
+        },
+
+        qualification: {
+            type: String,
+            enum: ["8th","10th", "12th", "iti", "diploma", "ug", "pg"],
+        },
+
+        skills: {
+            technical: [String],
+            soft: [String],
+        },
+
+        workExperience: [
+            {
+                jobTitle: String,
+                company: String,
+                years: Number,
+                description: String,
+            },
+        ],
+
+        certifications: [
+            {
+                courseName: String,
+                provider: String,
+                duration: String,
+                completionStatus: {
+                    type: String,
+                    enum: ["completed", "in-progress", "not-started"],
+                },
+
+            },
+        ],
+
+        preferredLanguages: [
+            {
+                type: String,
+                enum: ["english", "hindi", "tamil", "telugu", "kannada", "malayalam", "marathi", "bengali", "gujarati", "other"],
+            },
+        ],
+
+        learningAvailability : {
+            hoursPerWeek: {
+                type: Number,
+            },
+            preferredMode: {
+                type: String,
+                enum: ["online", "offline", "hybrid"],
+            },
+        },
+
+        learningPreferences: {
+            type: String,
+            enum: ["video", "reading", "hands-on", "mixed"],
+        },
+
+        isProfileComplete: {
+            type: Boolean,
+            default: false,
         },
     },
     {timestamps: true}
